454239b06da3ee611b46b691c6d1d1da405b0e43
Agraphie <real_freeren@yahoo.de>
2014-04-26 15:46:22 +0200

    meh

--- src/main/java/de/siobra/exampleapp/MongoConfig.java
+++ src/main/java/de/siobra/exampleapp/MongoConfig.java
@@ -16,17 +16,22 @@ public class MongoConfig extends AbstractMongoConfiguration{
 	 @Value("27017") private int port;
 	 @Value("exampleDB") private String databaseName;
 	 @Value("${MONGOSOUP_URL:}") private String mongoURL;
+	 private String ccDBName; 
 
 	@Override
 	protected String getDatabaseName() {
 		// TODO Auto-generated method stub
-		return "cc_tuOTCqVNyVgS";
+		if(ccDBName != null) {
+			return ccDBName;
+		}
+		return databaseName;
 	}
 
 	@Override
 	public Mongo mongo() throws Exception {
 		if(mongoURL != null && !mongoURL.isEmpty()){
 			MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoURL));
+			ccDBName = mongoClient.getDatabaseNames().get(0);
 			return mongoClient;
 		}
 	        return new MongoClient(host, port);
